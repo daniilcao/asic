17a3a54af15ddcdf724cbd75e5150554cf55b6aa
daniilcao <53031448+daniilcao@users.noreply.github.com>
2026-02-24 12:12:55 +0300

    Add Material-UI dark theme configuration

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+    background: {
+      default: '#121212',
+      paper: '#424242',
+    },
+  },
+});
+
+export default theme;
